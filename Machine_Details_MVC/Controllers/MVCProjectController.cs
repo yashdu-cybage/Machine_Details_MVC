4fffb3756411ae1e72306087976014330fd7054c
Yash Dattatray Dumbhare <yashdu@cybage.com>
2023-06-12 16:35:07 +0530

    Add project files.

---
+++ Machine_Details_MVC/Controllers/MVCProjectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http; 
+namespace MVC_Project.Controllers
+{
+    public class MVCProjectController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public MVCProjectController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var response = await _httpClient.GetAsync("https://localhost:7197/api/Machine");
+            var resultString = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject(resultString);
+            return View(result);
+        }
+    }
+}
---
+++ Machine_Details_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Machine_Details_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
